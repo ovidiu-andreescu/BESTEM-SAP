f64fbfc2ec8c94ec126d36dc53c5a7713a7aae58
ovidiu <ovidiu.andreescu@veridion.com>
2023-12-09 18:27:59 +0200

    bug fix

--- db_config/populate_db.py
+++ db_config/populate_db.py
@@ -9,5 +9,5 @@ def populate():
     sales_eod_path_df = pd.read_excel(sales_eod_path)
     transactions_df = pd.read_excel(transactions)
 
-    sales_eod_path_df.to_sql('sales_and_eod_stocks', engine, index=False, if_exists = 'replace')
-    transactions_df.to_sql('transactions', engine, index=False, if_exists = 'replace')
+    sales_eod_path_df.to_sql('sales_and_eod_stocks', engine, index=False, if_exists='replace')
+    transactions_df.to_sql('transactions', engine, index=False, if_exists='replace')
